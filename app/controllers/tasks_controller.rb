b78690f54c66279c3ff5d75274e062b6bf9a94fc
shunsaku.yokota <sadawo0126@gmail.com>
2020-02-25 08:16:18 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,6 +49,10 @@ class TasksController < ApplicationController
   
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
