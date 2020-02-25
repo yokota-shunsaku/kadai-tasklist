3397fa238e0b4fab582580b9c8a1c52a0f83115d
shunsaku.yokota <sadawo0126@gmail.com>
2020-02-25 07:18:10 +0000

    Add destroy tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,6 +39,14 @@ class TasksController < ApplicationController
     end
   end
   
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
   private
 
   # Strong Parameter
