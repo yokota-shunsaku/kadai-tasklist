5ac464e2025b107b76e3c452da8509c59156936a
shunsaku.yokota <sadawo0126@gmail.com>
2020-02-25 16:41:59 +0000

    for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 (1..100).each do |number|
-  Task.create!(status: 'test ' + number.to_s, content: 'test content ' + number.to_s)
+  Task.create(status: 't ' + number.to_s, content: 'test content ' + number.to_s)
 end
